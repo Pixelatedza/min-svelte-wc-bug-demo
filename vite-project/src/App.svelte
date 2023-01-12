50fd80f91df829541f849cb92022efd74bbf030e
justin.eloff <justine@bbdsoftware.com>
2023-01-12 21:35:09 +0200

    Setup minimal example

---
+++ vite-project/src/App.svelte
@@ -0,0 +1,9 @@
+<svelte:options tag='custom-element'/>
+
+<script>
+  import {someStore} from './store.js';
+  $: console.log($someStore);
+</script>
+
+<button on:click={() => someStore.update(n => n + 1)}>Change store value</button>
+<p>A minimal example of duplicate elements</p>
